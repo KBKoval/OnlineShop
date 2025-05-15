d456dfb6b84daa01cf987fb72e1b8aaa3585c8f6
Koval <koval@softlab.ru>
2025-05-15 06:35:22 +0300

    Step 1

--- src/main/java/com/koval/onlineshop/mappers/ItemMapper.java
+++ src/main/java/com/koval/onlineshop/mappers/ItemMapper.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 public class ItemMapper implements RowMapper<Item> {
     public Item mapRow(ResultSet rs, int rowNum) throws SQLException{
-        String idCloud = "https://mega.nz/file/"+rs.getString(rs.getString(2) );
-        return new Item(UUID.fromString(rs.getString(1)),rs.getString(2),idCloud,rs.getInt(3),rs.getFloat(4));
+        String idCloud = "https://mega.nz/file/"+rs.getString(rs.getString(3) );
+        return new Item(UUID.fromString(rs.getString(1)),rs.getString(2),idCloud,rs.getInt(4),rs.getFloat(5));
     }
 }
--- src/main/java/com/koval/onlineshop/model/Item.java
+++ src/main/java/com/koval/onlineshop/model/Item.java
@@ -1,17 +1,5 @@
 package com.koval.onlineshop.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.UUID;
-@AllArgsConstructor
-@Getter
-@Setter
-public class Item {
-    private UUID id;
-    private String description;
-    private String idCloud;
-    private int quantity;
-    private float price;
-}
+
+public record Item (UUID id,String description,String idCloud, int quantity, float price){}
