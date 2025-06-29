b32c74fa03eca5e357ea1e0419460244cb2b1af0
Koval <koval@softlab.ru>
2025-06-29 06:56:28 +0300

    Step 3

--- WebApp/src/main/java/com/koval/onlineshop/controllers/ShowcaseController.java
+++ WebApp/src/main/java/com/koval/onlineshop/controllers/ShowcaseController.java
@@ -1,14 +1,13 @@
 package com.koval.onlineshop.controllers;
 
 import com.koval.onlineshop.model.Item;
-import com.koval.onlineshop.services.interfaces.WorkingWithShowcase;
+
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 /*
@@ -21,12 +20,7 @@ import java.util.UUID;
 @CrossOrigin("*")
 @RequestMapping("api/katalog")
 public class ShowcaseController {
-    private final WorkingWithShowcase workingWithShowcase;
 
-    @Autowired
-    public ShowcaseController(WorkingWithShowcase workingWithShowcase) {
-        this.workingWithShowcase = workingWithShowcase;
-    }
 
     /**
      * Возвращает список товаров
@@ -35,17 +29,17 @@ public class ShowcaseController {
      */
     @GetMapping(value = "all")
     public HashMap<UUID, Item> getListCatalog() {
-        return workingWithShowcase.getListCatalog();
+        return  null;
     }
 
     /**
-     * аолучить  описание товара
+     * Получить  описание товара
      *
      * @param id идентификатор товара
      * @return описание
      */
     @GetMapping(value = "{id}")
     public String getDescription(@Valid @PathVariable("id") String id) {
-        return workingWithShowcase.getDescription(id);
+        return null;
     }
 }
